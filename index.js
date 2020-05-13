32ec3639338a3cce56d57015dc390a742810cb01
Diego Perea <diego.perea.dp@gmail.com>
2020-05-13 16:11:34 -0500

    ultimos cambios

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 var express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const routes = require('./src/routes/crmRoutes')
+const mongoose = require('mongoose');
 
-app.get('/', (req, res)=> {
-    res.send(`Node and Express server is running for port: ${PORT}`);
-
+mongoose.connect('mongodb://localhost/test', {
+    useNewUrlParser: true
+    
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
 })
+
+
+
+// // const Cat = mongoose.model('Cat', {name: String});
+
+// // const kitty = new Cat({ name: 'mimi'});
+
+// // kitty.save().then((res)=> {
+// //     console.log(res);
+// //     console.log('Meow');
+
+// // })
+
+// // // app.use(function(req, res, next){
+
+// // //     console.log(`Time`, Date.now());
+    
+
+// // // })
+
+// // app.get('/', function(req, res){
+// //     console.log(`Req Method: `, req.method)
+    
+
+// // // },  function(req, res, next){
+// // //     console.log(`Request Original Url`, req.originalUrl)
+// // //     next();
+
+// // // },  function(req, res, next){
+// // //     res.send(`Request successful`);
+// // })
