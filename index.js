5b358f74e50eba1e5dbef64915501986d76d4432
Diego Perea <diego.perea.dp@gmail.com>
2020-10-18 11:31:36 -0500

    feat(change): change port to 5000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const routes = require('./src/routes/crmRoutes')
 const mongoose = require('mongoose');
 
