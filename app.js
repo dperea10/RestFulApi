5f1cf24fd43cd9e3ece9618f8f80961bce4afaea
Diego Perea <diego.perea@payco.co>
2020-10-21 21:59:17 -0500

    feat(cambios): paquetes

---
+++ app.js
@@ -0,0 +1,48 @@
+const express  = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const routes = require('./src/routes/crmRoutes')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+})
+
+
+
+// // const Cat = mongoose.model('Cat', {name: String});
+
+// // const kitty = new Cat({ name: 'mimi'});
+
+// // kitty.save().then((res)=> {
+// //     console.log(res);
+// //     console.log('Meow');
+
+// // })
+
+// // // app.use(function(req, res, next){
+
+// // //     console.log(`Time`, Date.now());
+    
+
+// // // })
+
+// // app.get('/', function(req, res){
+// //     console.log(`Req Method: `, req.method)
+    
+
+// // // },  function(req, res, next){
+// // //     console.log(`Request Original Url`, req.originalUrl)
+// // //     next();
+
+// // // },  function(req, res, next){
+// // //     res.send(`Request successful`);
+// // })
